56a20057c71fc5b62d3cecdd9dfd8beede1d745e
karthini056 <karthinimuntha@gmail.com>
2025-09-13 15:13:33 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
+       <div className="dashboard-images">
+        <img src="https://images.unsplash.com/photo-1515168833906-d2a3b82b3029?auto=format&fit=crop&w=600&q=80" alt="Teamwork" title="Teamwork" />
+        <img src="https://images.unsplash.com/photo-1461749280684-dccba630e2f6?auto=format&fit=crop&w=600&q=80" alt="Online Payment" title="Online Payment" />
+        <img src="https://images.unsplash.com/photo-1504384308090-c894fdcc538d?auto=format&fit=crop&w=600&q=80" alt="Cloud Technology" title="Cloud Technology" />
+      </div>
       <SubscriptionDashboard />
     </div>
   );
